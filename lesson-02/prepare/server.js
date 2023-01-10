71efb5f3cf81885e1ffb7f9436a3c01de42b1ba8
spencer-rafada <spencer.byui@gmail.com>
2023-01-10 09:07:14 -0700

    process.env

--- lesson-02/prepare/server.js
+++ lesson-02/prepare/server.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
 const dotenv = require("dotenv");
+dotenv.config();
 const { MongoClient } = require("mongodb");
 
+const port = process.env.PORT || 3000;
+
 const main = async () => {
-  dotenv.config();
   const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pgivobk.mongodb.net/?retryWrites=true&w=majority`;
   console.log(connectionString);
   const client = new MongoClient(connectionString);
@@ -28,6 +30,7 @@ const listDatabases = async (client) => {
   });
 };
 
-app.listen(3000, () => {
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
   main();
 });
