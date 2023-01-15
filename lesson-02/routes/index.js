10d972859b28b7d21691d6182565e2f584afccd7
spencer-rafada <spencer.byui@gmail.com>
2023-01-14 19:15:44 -0700

    added default route instructions

--- lesson-02/routes/index.js
+++ lesson-02/routes/index.js
@@ -1,5 +1,11 @@
 const routes = require("express").Router();
 
+routes.get("/", (req, res, next) => {
+  res.json(
+    "Navigate to /contacts, then /contacts/{_id}, and /contacts/search?id={_id}"
+  );
+});
+
 routes.use("/contacts", require("./contacts"));
 
 module.exports = routes;
