f235e31549bd6857fa4559066abcf4da58d050f3
spencer-rafada <spencer.byui@gmail.com>
2023-01-18 18:35:27 -0700

    added middleware

--- lesson-02/server.js
+++ lesson-02/server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+// Middlewares
 var cors = require('cors');
+const bodyParser = require('body-parser');
+
 const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
 
+// Setting up middlewares
 app.use(cors());
-app.use('/', require('./routes'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(`/`, require(`./routes`));
 
 mongodb.initDb((err) => {
   if (err) {
